9bf2a72ffaf567c619f63abc57996176177debea
JULPACIS <julpacis11@gmail.com>
2025-08-07 13:40:37 +0800

    lab1

---
+++ app/Views/home.php
@@ -0,0 +1,6 @@
+<?= $this->extend('template') ?>
+
+<?= $this->section('content') ?>
+    <h1>Welcome to Pacis Home Page!</h1>
+    <p>This page is using a Bootstrap template Pacis.</p>
+<?= $this->endSection() ?>
