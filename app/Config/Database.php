fccacd0e9d715295dbe5e97a352475f697befb79
JULPACIS <julpacis11@gmail.com>
2025-08-13 22:53:19 +0800

    Databse loc

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -29,7 +29,7 @@ class Database extends Config
         'hostname'     => 'localhost',
         'username'     => '',
         'password'     => '',
-        'database'     => '',
+        'database'     => 'lms_Pacis',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
